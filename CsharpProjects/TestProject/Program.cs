2aec588e5122e98ff449efb33244a299f1c4c459
YandiYong <37985308+YandiYong@users.noreply.github.com>
2024-09-23 11:23:11 +0200

    largeNum

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello , C# I CAN I WILL I MUST!");
+﻿
+//This is the incorrect way to call the next().
+//int result = Random.Next(1, 7);
+
+int firstNo= 500;
+int secondNo= 600;
+
+int largeNo= Math.Max(firstNo,secondNo);    
+
+
+Console.WriteLine(largeNo);
